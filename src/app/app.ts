44a861bcf7bf5e8cbf907f14b2f2dc8473a21202
Kim Bum Jun <admin@vivabm.com>
2026-08-21 02:13:12 +0900

    feat:add menu-bar footer-bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBar } from "./pages/menu-bar/menu-bar";
+import { FooterBar } from "./pages/footer-bar/footer-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBar, FooterBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
